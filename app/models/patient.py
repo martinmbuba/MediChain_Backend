7cd1d12e29a9b2320efb6cd97f52e1ff8225e626
Umurowoto <umurowoto132@gmail.com>
2025-10-20 18:15:42 +0300

    patient model

---
+++ app/models/patient.py
@@ -0,0 +1,9 @@
+from db import db
+
+class Patient(db.Model):
+    __tablename__ = "patients"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
